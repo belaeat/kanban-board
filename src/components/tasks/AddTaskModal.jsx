0711143b2b9b1aacb9333aedef091c89706a2000
belaeat <omyhossain@gmail.com>
2023-09-08 22:18:38 +0600

    getting add task information correctly

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -1,13 +1,48 @@
+import { useForm } from "react-hook-form";
 import Modal from "../ui/Modal";
 
 const AddTaskModal = ({ isOpen, setIsOpen }) => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Add Task">
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo, aut,
-        suscipit sequi quae excepturi esse labore repellat amet illum unde totam
-        velit omnis nihil? Quod culpa perferendis cumque autem est.
-      </p>
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-4">
+        <div className="flex flex-col gap-4 mb-5">
+          <label htmlFor="title" className="font-semibold">
+            Title
+          </label>
+          <input
+            type="text"
+            id="title"
+            className="w-full rounded-md"
+            {...register("title")}
+          />
+        </div>
+        <div className="flex flex-col gap-4">
+          <label htmlFor="description" className="font-semibold">
+            Description
+          </label>
+          <textarea
+            type="text"
+            id="description"
+            className="w-full rounded-md "
+            {...register("description")}
+          />
+        </div>
+        <div className="flex gap-3 justify-end mt-5">
+          <button type="button" className="btn btn-danger">Cancel</button>
+          <button
+            type="submit"
+            className="btn btn-primary text-white font-semibold"
+          >
+            Add Task
+          </button>
+        </div>
+      </form>
     </Modal>
   );
 };
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -41,7 +41,7 @@ const Tasks = () => {
         <div className="grid grid-cols-3 gap-5 mt-10">
           <div className="relative h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
-              <h1>Up Next</h1>
+              <h1 className="font-bold">To Do</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
                 0
               </p>
@@ -52,7 +52,7 @@ const Tasks = () => {
           </div>
           <div className="relative h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
-              <h1>In Progress</h1>
+              <h1 className="font-bold">Doing</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
                 0
               </p>
@@ -64,7 +64,7 @@ const Tasks = () => {
           </div>
           <div className="relative h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
-              <h1>Up Next</h1>
+              <h1 className="font-bold">Done</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
                 0
               </p>
